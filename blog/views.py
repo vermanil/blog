17f24ef832f18b3fb2f30f535edd4a8ce22db177
vermanil <anilrajverma1996@gmail.com>
2016-08-12 11:23:39 +0530

    add css

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/home.html')
+    posts = post.objects.filter(title="first post")
+    return render(request, 'blog/home.html',{'posts':posts})
