5b94141a4d1af1df327e26cea7a65ab4ee78f5b2
vermanil <anilrajverma1996@gmail.com>
2017-01-24 11:15:03 +0530

    Add latest Article

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url, include
 from . import views
+
+app_name = 'blog'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    #url(r'^ind', views.latest, name='latest'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.http import HttpResponse
 from .models import post
 
 # Create your views here.
 def index(request):
-    posts = post.objects.filter(title="first post")
-    return render(request, 'blog/home.html',{'posts':posts})
+    posts = post.objects.all()
+    latestPost = post.objects.order_by('title', 'publish_date')
+    print("hello1")
+    print(latestPost)
+    print("hello")
+    return render(request, 'blog/home.html',{'posts':posts, 'latestPost': latestPost})
